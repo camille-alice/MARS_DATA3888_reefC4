968cb832bdfa85082d93d242aa3822fe0373c785
unknown <nche2098@uni.sydney.edu.au>
2022-05-10 14:40:32 +1000

    shiny regression plots

--- app/app.R
+++ app/app.R
@@ -27,12 +27,15 @@ reef_recent = reef_geomorphic %>%
          Diversity = as.numeric(Diversity)) %>%
   group_by(Reef.ID)
 
-reef_recent["rugosity"] = ifelse(reef_recent$class == "Reef Slope" | reef_recent$class == "Outer Reef Flat","High",
-                                 ifelse(reef_recent$class == "Sheltered Reef Slope" | reef_recent$class == "Reef Crest","Medium","Low"))
+reef_recent["rugosity"] = ifelse(reef_recent$class == "Reef Slope" | reef_recent$class == "Outer Reef Flat","High", ifelse(reef_recent$class == "Sheltered Reef Slope" | reef_recent$class == "Reef Crest" | reef_recent$class == "Back Reef Slope","Medium","Low"))
+reef_recent["rugosity_num"] = ifelse(reef_recent$class == "Reef Slope" | reef_recent$class == "Outer Reef Flat",3, ifelse(reef_recent$class == "Sheltered Reef Slope" | reef_recent$class == "Reef Crest" | reef_recent$class == "Back Reef Slope",2,1))
 
 reef_recent["bleached"] = ifelse(reef_recent$Average_bleaching > 0,1,0)
 
-reef_final = reef_recent %>% drop_na()
+reef_final = reef_recent %>% 
+  drop_na() %>% 
+  mutate(class = as.factor(class)) %>%
+  mutate(rugosity = as.factor(rugosity))
 
 # Function to plot the map, with variations based on the variables selected
 plot_map = function(var, start_date, end_date) {
@@ -41,7 +44,7 @@ plot_map = function(var, start_date, end_date) {
     filter(Date >= start_date) %>%
     filter(Date <= end_date)
   
-  if (var == "SSTA") {
+  if (var == "SSTA_Frequency_Standard_Deviation") {
     ggplot() + 
       geom_polygon(data = world_map, aes(x = long, y = lat, group = group), fill = "grey", alpha = 0.3) +
       geom_point(data = reef_temp, alpha = 0.2, aes(y = Latitude.Degrees, x = Longitude.Degrees, size = SSTA_Frequency_Standard_Deviation, color = SSTA_Frequency_Standard_Deviation)) + 
@@ -66,25 +69,62 @@ plot_map = function(var, start_date, end_date) {
   
 }
 
+# Function to plot linear regression, with variations based on the variables selected
+plot_regression = function(x, y, start_date, end_date) {
+  
+  reef_temp = reef_final %>%
+    filter(Date >= start_date) %>%
+    filter(Date <= end_date)
+  
+  title_string = paste(x, "vs", y)
+  
+  ggplot(reef_temp, aes(unlist(reef_temp[x]), unlist(reef_temp[y]))) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    labs(title = title_string, x = x, y = y)
+}
+
 # SHINY component
 ui = dashboardPage(
   dashboardHeader(title = "Reef C4"),
   dashboardSidebar(
-    sidebarMenu(
+    sidebarMenu(id = "menu",
       menuItem("Map", tabName = "Map"),
-      sliderInput("map_year", "Year Range", min = 1998, max = 2017, value = c(1998, 2017), sep = ""),
-      radioButtons("map_var", "Variable", choices = list("SSTA Frequency Standard Deviation" = "SSTA",
-                                                         "Diversity" = "Diversity",
-                                                         "Bleaching" = "Bleaching"))
+      menuItem("Plots", tabName = "Plots"),
+      conditionalPanel(
+        condition = 'input.menu == "Map"',
+        sliderInput("map_year", "Year Range", min = 1998, max = 2017, value = c(1998, 2017), sep = ""),
+        radioButtons("map_var", "Variable", choices = list("SSTA Frequency Standard Deviation" = "SSTA_Frequency_Standard_Deviation",
+                                                           "Diversity" = "Diversity",
+                                                           "Bleaching" = "Bleaching"))
+      ),
+      conditionalPanel(
+        condition = 'input.menu == "Plots"',
+        sliderInput("reg_year", "Year Range", min = 1998, max = 2017, value = c(1998, 2017), sep = ""),
+        radioButtons("reg_x", "X variable", choices = list("SSTA Frequency Standard Deviation" = "SSTA_Frequency_Standard_Deviation",
+                                                           "Diversity" = "Diversity",
+                                                           "Bleaching" = "Average_bleaching",
+                                                           "Depth" = "Depth"), selected = "SSTA_Frequency_Standard_Deviation"),
+        radioButtons("reg_y", "Y variable", choices = list("SSTA Frequency Standard Deviation" = "SSTA_Frequency_Standard_Deviation",
+                                                             "Diversity" = "Diversity",
+                                                             "Bleaching" = "Average_bleaching",
+                                                             "Depth" = "Depth"), selected = "Diversity")
+      )
     )
   ),
   dashboardBody(
     tabItems(
-      # First tab content
       tabItem(tabName = "Map",
         fluidRow(
           box(
             plotOutput("map")
+          )
+        )
+      ),
+      tabItem(tabName = "Plots",
+        fluidRow(
+          box(
+            plotOutput("regression")
           )
         )
       )
@@ -97,6 +137,11 @@ server = function(input, output) {
   # Initial map
   output$map = renderPlot({
     plot_map(input$map_var, input$map_year[1], input$map_year[2])
+  })
+  
+  # Regression plots
+  output$regression = renderPlot({
+    plot_regression(input$reg_x, input$reg_y, input$reg_year[1], input$reg_year[2])
   })
   
 }
